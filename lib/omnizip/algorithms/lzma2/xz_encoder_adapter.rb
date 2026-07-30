dae4574f7db2d7badb2e6b31d58b47c29ee84869
HassanAkbar <hassanakbar2@gmail.com>
2026-07-30 13:50:48 +0500

    require stringio and tempfile where referenced

--- lib/omnizip/algorithms/lzma2/xz_encoder_adapter.rb
+++ lib/omnizip/algorithms/lzma2/xz_encoder_adapter.rb
@@ -20,6 +20,8 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+require "stringio"
+
 module Omnizip
   module Algorithms
     # Adapter for XZ Encoder to work with LZMA2 chunking
--- lib/omnizip/algorithms/ppmd_base.rb
+++ lib/omnizip/algorithms/ppmd_base.rb
@@ -20,6 +20,8 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 # DEALINGS IN THE SOFTWARE.
 
+require "stringio"
+
 module Omnizip
   module Algorithms
     # Base class for PPMd (Prediction by Partial Matching) algorithms
